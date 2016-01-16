56ebc71d312a046993afc70e46b2eb3329d9f000
mrida <mrida88@gmail.com>
2016-01-16 18:17:52 +0200

    Create an empty application

---
+++ src/yam/root/rescue/Rescue.java
@@ -0,0 +1,12 @@
+package yam.root.rescue;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Rescue extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
