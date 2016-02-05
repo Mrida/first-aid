c37c0d3caa787e3f2b49c515aa674dd9fb6d9478
mrida <mrida88@gmail.com>
2016-02-05 21:34:16 +0200

    Create client and server connection threads: - the device tries to connect to a server (another device) and if it fails, it acts as the server - the second connecting device will be acting as a client - change project version to android19 to make it compatible with android 4.4.2

--- src/yam/root/utilities/BluetoothManager.java
+++ src/yam/root/utilities/BluetoothManager.java
@@ -2,15 +2,23 @@ package yam.root.utilities;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import static yam.root.ApplicationContext.showShortMessage;
 
 public class BluetoothManager {
+    public static final UUID CONNECTION_UUID = UUID.fromString("45743cf6-c787-11e5-9956-625662870761");
+    public static final String APPLICATION_NAME = "Rescue";
+
     private Context context;
     private BluetoothAdapter bluetoothAdapter;
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
@@ -28,7 +36,7 @@ public class BluetoothManager {
 
     private void handleDevices(Intent intent) {
         BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-        showShortMessage(device.getAddress());
+        new ConnectionThread(device).start();
     }
 
     public void enableBluetoothAndConnect() {
@@ -59,4 +67,68 @@ public class BluetoothManager {
     public void onDestroy() {
         context.unregisterReceiver(broadcastReceiver);
     }
+
+    private class ConnectionThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+        private BluetoothDevice otherDevice;
+
+        public ConnectionThread(BluetoothDevice device) {
+            otherDevice = device;
+            BluetoothServerSocket tmp = null;
+            try {
+                tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(APPLICATION_NAME, CONNECTION_UUID);
+            } catch (IOException ignored) {
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+
+            try {
+                connectAsClient();
+            } catch (IOException ex) {
+                BluetoothSocket socket;
+                while (true) {
+                    try {
+                        socket = mmServerSocket.accept();
+                    } catch (IOException e) {
+                        break;
+                    }
+                    if (socket != null) {
+                        try {
+                            mmServerSocket.close();
+                        } catch (IOException ignored) {
+                        }
+                        manageClientSocket(socket);
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        private void connectAsClient() throws IOException {
+            BluetoothSocket otherServerSocket = otherDevice.createRfcommSocketToServiceRecord(CONNECTION_UUID);
+            try {
+                otherServerSocket.connect();
+            } catch (IOException connectException) {
+                otherServerSocket.close();
+                throw new IOException(connectException);
+            }
+
+            manageOtherServerSocket(otherServerSocket);
+        }
+
+
+        private void manageClientSocket(BluetoothSocket client) {
+            showShortMessage("I am the server and here I contact my client");
+        }
+
+        private void manageOtherServerSocket(BluetoothSocket server) {
+
+            showShortMessage("I am the client and here I contact my server");
+        }
+    }
 }
+
+
